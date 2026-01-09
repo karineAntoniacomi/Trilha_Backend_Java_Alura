9d4e9da3c713d315fc72b2086a3e9639931704d3
Karine <karine.antoniacomi@gmail.com>
2026-01-09 12:46:22 -0300

    feat: Boas práticas de programação: melhore o código de uma API Java - aplicando padrão DTO na Service

--- boas-praticas-java-projeto_inicial/src/main/java/br/com/alura/adopet/api/controller/AdocaoController.java
+++ boas-praticas-java-projeto_inicial/src/main/java/br/com/alura/adopet/api/controller/AdocaoController.java
@@ -1,13 +1,20 @@
 package br.com.alura.adopet.api.controller;
 
+import br.com.alura.adopet.api.dto.AprovacaoAdocaoDto;
+import br.com.alura.adopet.api.dto.DadosDetalhesPet;
+import br.com.alura.adopet.api.dto.ReprovacaoAdocaoDto;
+import br.com.alura.adopet.api.dto.SolicitacaoAdocaoDto;
 import br.com.alura.adopet.api.exception.ValidacaoException;
-import br.com.alura.adopet.api.model.Adocao;
+import br.com.alura.adopet.api.model.Pet;
 import br.com.alura.adopet.api.service.AdocaoService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/adocoes")
@@ -16,11 +23,23 @@ public class AdocaoController {
     @Autowired
     private AdocaoService adocaoService;
 
+    @GetMapping
+    public ResponseEntity<List<DadosDetalhesPet>> listarTodosDisponiveis() {
+        List<Pet> pets = repository.findAll();
+        List<DadosDetalhesPet> disponiveis = new ArrayList<>();
+        for (Pet pet : pets) {
+            if (pet.getAdotado() == false) {
+                disponiveis.add(new DadosDetalhesPet(pet));
+            }
+        }
+        return ResponseEntity.ok(disponiveis);
+    }
+
     @PostMapping
     @Transactional
-    public ResponseEntity<String> solicitar(@RequestBody @Valid  Adocao adocao) {
+    public ResponseEntity<String> solicitar(@RequestBody @Valid SolicitacaoAdocaoDto dto) {
         try {
-            this.adocaoService.solicitar(adocao);
+            this.adocaoService.solicitar(dto);
             return ResponseEntity.ok("Adoção solicitada com sucesso!");
         } catch (ValidacaoException e) {
             return ResponseEntity.badRequest().body(e.getMessage());
@@ -29,15 +48,15 @@ public class AdocaoController {
 
     @PutMapping("/aprovar")
     @Transactional
-    public ResponseEntity<String> aprovar(@RequestBody @Valid Adocao adocao) {
-        this.adocaoService.aprovar(adocao);
+    public ResponseEntity<String> aprovar(@RequestBody @Valid AprovacaoAdocaoDto dto) {
+        this.adocaoService.aprovar(dto);
         return ResponseEntity.ok().build();
     }
 
     @PutMapping("/reprovar")
     @Transactional
-    public ResponseEntity<String> reprovar(@RequestBody @Valid Adocao adocao) {
-        this.adocaoService.reprovar(adocao);
+    public ResponseEntity<String> reprovar(@RequestBody @Valid ReprovacaoAdocaoDto dto) {
+        this.adocaoService.reprovar(dto);
         return ResponseEntity.ok().build();
     }
 
