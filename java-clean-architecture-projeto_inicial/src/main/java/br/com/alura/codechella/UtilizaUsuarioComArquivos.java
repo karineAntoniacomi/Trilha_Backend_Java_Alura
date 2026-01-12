0c750b055393a67d45590c0cb51a129a6e8374fc
Karine <karine.antoniacomi@gmail.com>
2026-01-12 16:16:02 -0300

    feat: clean arch - persistindo dados em outro formato

---
+++ java-clean-architecture-projeto_inicial/src/main/java/br/com/alura/codechella/UtilizaUsuarioComArquivos.java
@@ -0,0 +1,22 @@
+package br.com.alura.codechella;
+
+import br.com.alura.codechella.domain.entities.usuario.Usuario;
+import br.com.alura.codechella.infra.gateways.RepositorioDeUsuarioEmArquivo;
+
+import java.io.IOException;
+import java.time.LocalDate;
+
+public class UtilizaUsuarioComArquivos {
+
+    public static void main(String[] args) throws IOException {
+        RepositorioDeUsuarioEmArquivo repositorioDeUsuarioEmArquivo = new RepositorioDeUsuarioEmArquivo();
+
+        repositorioDeUsuarioEmArquivo.cadastrarUsuario(new Usuario("456.789.888-88", "João", LocalDate.parse("2000-10-15"), "joao@email.com"));
+        repositorioDeUsuarioEmArquivo.cadastrarUsuario(new Usuario("456.789.888-88", "Maria", LocalDate.parse("2000-10-15"), "maria@email.com"));
+        repositorioDeUsuarioEmArquivo.cadastrarUsuario(new Usuario("456.789.888-88", "Vinicios", LocalDate.parse("2000-10-15"), "vinicios@email.com"));
+        repositorioDeUsuarioEmArquivo.cadastrarUsuario(new Usuario("456.789.888-88", "Raphael", LocalDate.parse("2000-10-15"), "raphael@email.com"));
+
+       // System.out.println(repositorioDeUsuarioEmArquivo.listarTodos());
+        repositorioDeUsuarioEmArquivo.gravaEmArquivo("usuarios.txt");
+    }
+}
---
+++ java-clean-architecture-projeto_inicial/src/main/java/br/com/alura/codechella/infra/gateways/RepositorioDeUsuarioEmArquivo.java
@@ -0,0 +1,41 @@
+package br.com.alura.codechella.infra.gateways;
+
+import br.com.alura.codechella.application.gateways.RepositorioDeUsuario;
+import br.com.alura.codechella.domain.entities.usuario.Usuario;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RepositorioDeUsuarioEmArquivo implements RepositorioDeUsuario {
+
+    List<Usuario> usuarios = new ArrayList<>();
+
+    @Override
+    public Usuario cadastrarUsuario(Usuario usuario) {
+        this.usuarios.add(usuario);
+        return usuario;
+    }
+
+    @Override
+    public List<Usuario> listarTodos() {
+        return this.usuarios;
+    }
+
+    public void gravaEmArquivo(String nomeArquivo) throws IOException {
+        FileWriter fileWriter = new FileWriter(nomeArquivo);
+        fileWriter.write(this.usuarios.toString());
+        fileWriter.close();
+    }
+
+    @Override
+    public Usuario alteraUsuario(String cpf, Usuario usuario) {
+        return null;
+    }
+
+    @Override
+    public void excluiUsuario(String cpf) {
+
+    }
+}
