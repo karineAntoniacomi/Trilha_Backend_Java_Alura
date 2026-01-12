2a90001c33f3334c0cf3b43c8310fef52ab69555
Karine <karine.antoniacomi@gmail.com>
2026-01-12 20:26:32 -0300

    feat: Arquitetura Java: modelando aplicações com Domain-Driven Design - agregados e fabrica de eventos

--- java-domain-driven-design-main/src/main/java/br/com/alura/codechella/dominio/evento/Endereco.java
+++ java-domain-driven-design-main/src/main/java/br/com/alura/codechella/dominio/evento/Endereco.java
@@ -5,4 +5,28 @@ public class Endereco {
     private String cep;
     private Integer numero;
     private String complemento;
+
+    public Endereco(String cep, Integer numero, String complemento) {
+        this.cep = cep;
+        this.numero = numero;
+        this.complemento = complemento;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public Integer getNumero() {
+        return numero;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Cep: %s, número %s - complemento %s",
+                cep, numero, complemento);
+    }
 }
