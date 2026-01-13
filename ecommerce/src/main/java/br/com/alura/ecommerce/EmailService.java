1fb6d40232b59d5c1d5343797e32a03f0ba4c3b9
Karine <karine.antoniacomi@gmail.com>
2026-01-13 16:00:37 -0300

    Kafka curso 1: produtores, consumidores e streams - extraindo camada producer

--- ecommerce/src/main/java/br/com/alura/ecommerce/EmailService.java
+++ ecommerce/src/main/java/br/com/alura/ecommerce/EmailService.java
@@ -1,20 +1,17 @@
 package br.com.alura.ecommerce;
 
-import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.apache.kafka.common.serialization.StringDeserializer;
 
-import java.time.Duration;
-import java.util.Collections;
-import java.util.Properties;
+import java.io.IOException;
 
 public class EmailService {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         var emailService = new EmailService();
-        var service = new KafkaService(EmailService.class.getSimpleName(), "ECOMMERCE_SEND_EMAIL", emailService::parse);
-        service.run();
+
+        try (var service = new KafkaService(EmailService.class.getSimpleName(), "ECOMMERCE_SEND_EMAIL", emailService::parse)) {
+            service.run();
+        }
     }
 
     private void parse(ConsumerRecord<String, String> record) {
