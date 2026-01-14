be9833b6d5f214d269ae4e906c23edfb43c02898
Karine <karine.antoniacomi@gmail.com>
2026-01-14 00:56:05 -0300

    Kafka curso 2: Fast delegate, evolução e cluster de brokers - novos produtores e consumidores

--- ecommerce/common-kafka/src/main/java/ConsumerFunction.java
+++
@@ -1,6 +0,0 @@
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-
-public interface ConsumerFunction<T> {
-
-    void consume(ConsumerRecord<String, T> record);
-}
---
+++ kafka-introducao-e-microssservicos/ecommerce/service-new-order/src/main/java/br/com/alura/ecommerce/NewOrderMain.java
@@ -0,0 +1,31 @@
+package br.com.alura.ecommerce;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+import java.util.concurrent.ExecutionException;
+
+public class NewOrderMain {
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+
+        // fecha o dispatcher caso ocorra alguma excessão
+        try (var orderDispatcher = new KafkaDispatcher<Order>()) {
+            try (var emailDispatcher = new KafkaDispatcher<String>()) {
+                for (var i = 0; i < 10; i++) {
+                    // Chave que decide em qual partição irá cair a mensagem
+                    var userId = UUID.randomUUID().toString(); // aleatoriza a chave para não cair sempre na mesma partição
+                    var orderId = UUID.randomUUID().toString();
+                    var amount = new BigDecimal(Math.random() * 5000 + 1);  // Math.random devolve double, por isso é tranformando em bigDecimal
+
+                    var order = new Order(userId, orderId, amount);
+                    // envio
+                    orderDispatcher.send("ECOMMERCE_NEW_ORDER", userId, order);
+
+                    var email = "Thank you for your order! We are processing your order!";
+                    emailDispatcher.send("ECOMMERCE_SEND_EMAIL", userId, email);
+                }
+            }
+        }
+    }
+
+}
